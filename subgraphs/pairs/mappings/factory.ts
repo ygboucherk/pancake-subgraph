75049b823753cb7a7458059a55219bb4a0db1460
ygboucherk <ygboucherk@gmail.com>
2021-08-28 21:11:54 +0200

    Update factory.ts

--- subgraphs/pairs/mappings/factory.ts
+++ subgraphs/pairs/mappings/factory.ts
@@ -5,7 +5,7 @@ import { PairCreated } from "../generated/Factory/Factory";
 import { fetchTokenDecimals, fetchTokenName, fetchTokenSymbol } from "./utils/erc20";
 
 // Constants
-let FACTORY_ADDRESS = "0xca143ce32fe78f1f7019d7d551a6402fc5350c73";
+let FACTORY_ADDRESS = "0xd4B3d26190CBb9bA81A680a5A27C0902be66390a";
 
 // BigNumber-like references
 let ZERO_BI = BigInt.fromI32(0);
